be0d1429517528317b487a3845102be42f7052ff
syamilAbdillah <abdillahsyamil77@gmail.com>
2020-09-16 14:57:37 +0700

    final bgt nih

---
+++ data.js
@@ -0,0 +1,729 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-gs__0048",
+      "name": "GS__0048",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.20881524991339262,
+          "pitch": 0.16522838405167484,
+          "rotation": 18.84955592153877,
+          "target": "1-gs__0054"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.2189766124392083,
+          "pitch": -0.02147261279002244,
+          "title": "Masuk&nbsp;",
+          "text": "Untuk Melihat interior"
+        },
+        {
+          "yaw": 0.3061051126408003,
+          "pitch": -0.43331554943944184,
+          "title": "Contoh Rumah",
+          "text": "&nbsp;"
+        },
+        {
+          "yaw": -0.1626614404454223,
+          "pitch": 0.4613657856723119,
+          "title": "Garasi",
+          "text": "&nbsp;"
+        }
+      ]
+    },
+    {
+      "id": "1-gs__0054",
+      "name": "GS__0054",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.06564417996015415,
+          "pitch": 0.6046918990547052,
+          "rotation": 6.283185307179586,
+          "target": "2-gs__0056"
+        },
+        {
+          "yaw": -1.3330855425549206,
+          "pitch": 0.365172430342497,
+          "rotation": 0,
+          "target": "3-gs__0057"
+        },
+        {
+          "yaw": 0.3107665858751556,
+          "pitch": 0.2861552146078328,
+          "rotation": 0,
+          "target": "10-gs__0064"
+        },
+        {
+          "yaw": -0.6206685738681124,
+          "pitch": 0.23442101608709365,
+          "rotation": 0,
+          "target": "11-gs__0059"
+        },
+        {
+          "yaw": -2.8253355183873623,
+          "pitch": 0.078496566591852,
+          "rotation": 5.497787143782138,
+          "target": "0-gs__0048"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.3312256326639726,
+          "pitch": 0.04274986160404204,
+          "title": "Kamar 1",
+          "text": "Lantai 1"
+        },
+        {
+          "yaw": 0.27265262080155317,
+          "pitch": 0.02270724058717377,
+          "title": "Dapur",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.6206694436639246,
+          "pitch": 0.00026404243557642815,
+          "title": "Kamar 2",
+          "text": "Lantai 1"
+        },
+        {
+          "yaw": 1.4746274082649595,
+          "pitch": 0.14867567524704484,
+          "title": "Ruang tamu",
+          "text": "Lantai 1"
+        },
+        {
+          "yaw": -2.703719887625274,
+          "pitch": -0.14184137909252215,
+          "title": "Keluar",
+          "text": "&nbsp;"
+        }
+      ]
+    },
+    {
+      "id": "2-gs__0056",
+      "name": "GS__0056",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6560881960041076,
+          "pitch": 0.6190094218977578,
+          "rotation": 0,
+          "target": "1-gs__0054"
+        },
+        {
+          "yaw": -1.3357256400887358,
+          "pitch": 0.10510365686187839,
+          "rotation": 0,
+          "target": "0-gs__0048"
+        },
+        {
+          "yaw": -0.6981439996169527,
+          "pitch": 0.4340238595542836,
+          "rotation": 0,
+          "target": "3-gs__0057"
+        },
+        {
+          "yaw": 0.24335800486798753,
+          "pitch": 0.32690814856270833,
+          "rotation": 0,
+          "target": "4-gs__0058"
+        },
+        {
+          "yaw": -0.11771413153548949,
+          "pitch": -0.18547627400920774,
+          "rotation": 0.7853981633974483,
+          "target": "5-gs__0060"
+        },
+        {
+          "yaw": 2.2509214131911364,
+          "pitch": 0.20635787465444722,
+          "rotation": 0,
+          "target": "10-gs__0064"
+        },
+        {
+          "yaw": 0.5660499541030486,
+          "pitch": 0.330462024551581,
+          "rotation": 0,
+          "target": "11-gs__0059"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.6793406906953425,
+          "pitch": 0.2560407633359958,
+          "title": "Kamar 1",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.22173663080711847,
+          "pitch": 0.156751097498951,
+          "title": "Toilet Bawah",
+          "text": "lantai 1"
+        },
+        {
+          "yaw": 2.2027822714695215,
+          "pitch": -0.15706114908852342,
+          "title": "Dapur",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.1354724118880526,
+          "pitch": -0.3349999603435787,
+          "title": "Tangga&nbsp;",
+          "text": "Menuju Lantai 2"
+        },
+        {
+          "yaw": 0.5019101415057019,
+          "pitch": 0.05864633847393286,
+          "title": "Kamar 2",
+          "text": "Lantai 1"
+        },
+        {
+          "yaw": -1.745370975884267,
+          "pitch": 0.12505762869580472,
+          "title": "Ruang Tamu",
+          "text": "Lantai 1"
+        }
+      ]
+    },
+    {
+      "id": "3-gs__0057",
+      "name": "GS__0057",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5555004829569512,
+          "pitch": 0.42875729054772904,
+          "rotation": 0,
+          "target": "1-gs__0054"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.5447348350818224,
+          "pitch": 0.25343235181711243,
+          "title": "Ruang Tamu",
+          "text": "Lantai 1"
+        },
+        {
+          "yaw": 2.6562427049054893,
+          "pitch": -0.07825385138179897,
+          "title": "Kamar 1&nbsp;",
+          "text": "Lantai 1"
+        }
+      ]
+    },
+    {
+      "id": "4-gs__0058",
+      "name": "GS__0058",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.058782013260302,
+          "pitch": 0.1354464924370582,
+          "rotation": 0.7853981633974483,
+          "target": "2-gs__0056"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.543670876086292,
+          "pitch": 0.1901643171512326,
+          "title": "Toilet 1",
+          "text": "Lantai 1"
+        },
+        {
+          "yaw": -3.0470365762437837,
+          "pitch": -0.02881092411942987,
+          "title": "Ruang tamu",
+          "text": "Lantai 1"
+        }
+      ]
+    },
+    {
+      "id": "5-gs__0060",
+      "name": "GS__0060",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.34402005363261345,
+          "pitch": -0.5386811151576723,
+          "rotation": 0,
+          "target": "6-gs__0061"
+        },
+        {
+          "yaw": 0.5561802296885396,
+          "pitch": 0.641189134624863,
+          "rotation": 0,
+          "target": "2-gs__0056"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.3217659877081829,
+          "pitch": -0.7531052157297324,
+          "title": "Lantai 2",
+          "text": "Menuju kamar ke-3"
+        },
+        {
+          "yaw": 0.5405745534203596,
+          "pitch": 0.38978342651789966,
+          "title": "Lantai 1",
+          "text": "Menuju ruang tamu"
+        }
+      ]
+    },
+    {
+      "id": "6-gs__0061",
+      "name": "GS__0061",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.20756301798497034,
+          "pitch": 0.3431275950414623,
+          "rotation": 0,
+          "target": "7-gs__0062"
+        },
+        {
+          "yaw": 0.5310261016371953,
+          "pitch": 0.793871372931509,
+          "rotation": 0,
+          "target": "8-gs__0063"
+        },
+        {
+          "yaw": 2.7309223729759378,
+          "pitch": 0.3914571520894867,
+          "rotation": 4.71238898038469,
+          "target": "5-gs__0060"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.20987380027607117,
+          "pitch": 0.16544945469114225,
+          "title": "Kamar 3",
+          "text": "Lantai 2"
+        },
+        {
+          "yaw": 2.6815414769198975,
+          "pitch": 0.17487086997254053,
+          "title": "Tangga&nbsp;",
+          "text": "Menuju lantai 1"
+        }
+      ]
+    },
+    {
+      "id": "7-gs__0062",
+      "name": "GS__0062",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.883105417289661,
+          "pitch": 0.14532375308191092,
+          "rotation": 5.497787143782138,
+          "target": "6-gs__0061"
+        },
+        {
+          "yaw": 3.139979158450595,
+          "pitch": 0.45749707754500335,
+          "rotation": 0,
+          "target": "8-gs__0063"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.901459210313833,
+          "pitch": 0.12906416105391472,
+          "title": "Kamar mandi",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.5804627392181239,
+          "pitch": 0.14233096739131312,
+          "title": "Kamar 2",
+          "text": "Lantai 2"
+        }
+      ]
+    },
+    {
+      "id": "8-gs__0063",
+      "name": "GS__0063",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7958967987843177,
+          "pitch": 0.4592087136378762,
+          "rotation": 0,
+          "target": "7-gs__0062"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.0010859663477589265,
+          "pitch": 0,
+          "title": "Toilet 2&nbsp;",
+          "text": "Didalam kamar 3&nbsp;"
+        },
+        {
+          "yaw": -1.7771118091553415,
+          "pitch": 0.27375475712440256,
+          "title": "Kamar 3",
+          "text": "Lantai 2"
+        }
+      ]
+    },
+    {
+      "id": "9-gs__0064",
+      "name": "GS__0064",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-gs__0064",
+      "name": "GS__0064",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1848658268370933,
+          "pitch": 0.41644005776745807,
+          "rotation": 0,
+          "target": "2-gs__0056"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.1941985282777239,
+          "pitch": 0.1804370037472811,
+          "title": "Ruang tamu",
+          "text": "Lantai 1"
+        },
+        {
+          "yaw": -0.6352011508720175,
+          "pitch": 0.0005316774931216628,
+          "title": "Dapur",
+          "text": "Lantai 1"
+        }
+      ]
+    },
+    {
+      "id": "11-gs__0059",
+      "name": "GS__0059",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.34433987403661,
+          "pitch": 0.20744580711645888,
+          "rotation": 0,
+          "target": "1-gs__0054"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.365328390180853,
+          "pitch": 0.024300523648083328,
+          "title": "Ruang Tamu",
+          "text": "Lantai 1"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
